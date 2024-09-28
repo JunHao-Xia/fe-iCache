4013929886daeb508f1e4d44a6d6d5c53cea90c5
zhangz1w3nCode <403592973@qq.com>
2024-09-29 04:19:51 +0800

    feat:project init.

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+6
+</template>
+
+<script setup>
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<template>
+  <a-layout style="min-height: 100vh">
+
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <RiseOutlined/>
+          <span>Logo</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <pie-chart-outlined />
+              <span>Cache</span>
+            </span>
+          </template>
+          <a-menu-item key="2">Cache Monitor</a-menu-item>
+          <a-menu-item key="3">Cache Refresh</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+
+    <a-layout>
+      <a-layout-content style="margin: 0 16px">
+        <demo></demo>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ocean Platform Created By Zhangz1w3nCode.
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script lang="ts" setup>
+import {
+  PieChartOutlined,
+  RiseOutlined
+} from '@ant-design/icons-vue';
+import { ref } from 'vue';
+import Demo from "../components/demo.vue";
+const collapsed = ref<boolean>(false);
+const selectedKeys = ref<string[]>(['1']);
+</script>
+<style scoped>
+#components-layout-demo-side .logo {
+  height: 32px;
+  margin: 16px;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
+}
+</style>
