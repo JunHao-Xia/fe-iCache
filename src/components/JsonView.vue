6a5b7085b438c1ced898314449abe218ad93b265
zhangz1w3nCode <403592973@qq.com>
2024-10-03 02:24:59 +0800

    feat:add cache key list view.

---
+++ src/components/JsonView.vue
@@ -0,0 +1,28 @@
+<template>
+  <vue-json-viewer :value="cacheValue"
+                   show-double-quotes
+                   boxed
+                   sort
+                   copyable
+  >
+  </vue-json-viewer>
+</template>
+
+<script setup>
+import VueJsonViewer from "vue-json-viewer"
+import {ref} from "vue";
+let cacheValue = ref({
+  "Data": {
+    "ProductId": 1,
+    "ProductName": "MockProductName_1",
+    "ProductPrice": 2000,
+    "ProductSku": 1001
+  },
+  "WriteTime": 1727885796,
+  "AccessTime": 1727885796,
+  "CacheValueMetrics": {
+    "CacheSize": 16,
+    "CacheHitCount": 1
+  }
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css';
+import VueJsonViewer from 'vue-json-viewer'
 
 const app = createApp(App)
 app.use(router)
 app.use(Antd)
+app.use(VueJsonViewer)
 app.mount('#app')
