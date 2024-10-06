2d12055c939fb7ad1f00796d04e42ea8a51b3b46
zhangz1w3nCode <403592973@qq.com>
2024-10-07 02:03:41 +0800

    feat:add cache cache monitor ability.

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import MonitorView from "../views/MonitorView.vue";
+import MonitorCenter from "../views/MonitorCenter.vue";
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/about', component: AboutView },
     { path: '/monitor', component: MonitorView },
+    { path: '/center', component: MonitorCenter },
 ]
 
 const router = createRouter({
