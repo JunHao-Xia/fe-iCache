71f177605dd9e59af46a056ddbc0d746f2ac4cfa
zhangz1w3nCode <403592973@qq.com>
2025-01-26 03:06:42 +0800

    feat:add logic flow framwork.

--- src/api/flowProcess.js
+++ src/api/flowProcess.js
@@ -1,6 +1,20 @@
 import requestUtils from './http/request';
+
 export function saveFlow() {
   return requestUtils.post({
     url: '/flow/saveFlow'
+  })
+}
+export function toEL(param) {
+  console.log(param)
+  return requestUtils.post({
+    url: '/flow/toEl',
+    data: param
+  })
+}
+
+export function getProcessNodeList() {
+  return requestUtils.get({
+    url: '/flow/getProcessNodeList'
   })
 }
