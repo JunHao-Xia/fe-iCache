a7c6311d804c9fdc7a75ad0384de9d8ee4f69720
zhangz1w3nCode <403592973@qq.com>
2025-01-26 00:25:38 +0800

    feat:add logic flow framwork.

--- src/views/flowProcess/FlowDesignView.vue
+++ src/views/flowProcess/FlowDesignView.vue
@@ -1,23 +1,126 @@
 <template>
-  <div style="padding: 20px; display: flex; flex-direction: column; height: 100vh;">
-    <div style="display: flex; align-items: center; margin-bottom: 20px;">
-
-    </div>
-
-    <div style="display: flex; gap: 20px; flex-grow: 1;">
-      <div style="flex: 1; min-width: 200px; max-height: calc(100vh - 150px);">
-
-      </div>
-      <div style="flex: 2; min-width: 620px; max-height: calc(100vh - 150px);">
-
-      </div>
+  <div class="drawArea">
+    <div class="container" ref="container">
     </div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+//样式
+import "@logicflow/core/dist/style/index.css";
+import '@logicflow/extension/lib/style/index.css'
+
+//插件组件
+import LogicFlow from "@logicflow/core";
+import {Highlight,DndPanel,Menu,Control,MiniMap,SelectionSelect,InsertNodeInPolyline,Snapshot } from '@logicflow/extension';
+
+//插件注册 方式1
+// LogicFlow.use(Snapshot);
+// LogicFlow.use(InsertNodeInPolyline);
+// LogicFlow.use(SelectionSelect);
+// LogicFlow.use(MiniMap);
+// LogicFlow.use(Menu);
+// LogicFlow.use(Control);
+// LogicFlow.use(DndPanel);
+
+
+export default {
+
+  mounted() {
+    this.lf = new LogicFlow({
+      //插件注册 方式2
+      plugins: [Highlight,DndPanel,Menu,Control,MiniMap,SelectionSelect,InsertNodeInPolyline,Snapshot],
+
+      container: this.$refs.container,
+      //是否显示网格
+      grid: true,
+      stopMoveGraph: true,
+      //开启快捷键
+      keyboard: {
+        enabled: true,
+      },
+      //边类型 'line' | 'polyline' | 'bezier'
+      edgeType: 'line',
+      //对齐线 只有网格属性打开才能显示对齐线
+      snapline: true
+    });
+    this.lf.render();
+    //自定义对齐线样式
+    this.lf.setTheme({
+      snapline: {
+        stroke: '#1E90FF', // 对齐线颜色
+        strokeWidth: 1, // 对齐线宽度
+      },
+    })
+    this.lf.extension.selectionSelect.openSelectionSelect();
+    this.lf.extension.selectionSelect.setSelectionSense(false, false);
+
+    //*自定义拖拽面板内容 业务节点要注册到这
+    this.lf.extension.dndPanel.setPatternItems([
+      {
+        type: 'circle',
+        text: '',
+        label: '开始节点',
+        icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg==',
+      },
+      {
+        type: 'polygon',
+        text: '',
+        label: '判断节点',
+        icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg==',
+      },
+      {
+        type: 'rect',
+        text: '',
+        label: '业务节点',
+        icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg==',
+      },
+      {
+        type: 'circle',
+        text: '',
+        label: '结束节点',
+        icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg==',
+      }
+    ]);
+
+    //自定义控制面板 待开发
+    // this.lf.extension.control.addItem({
+    //   key: 'mini-map',
+    //   iconClass: 'custom-minimap',
+    //   title: '',
+    //   text: '导航',
+    //   onMouseEnter: (lf, ev) => {
+    //     const position = lf.getPointByClient(ev.x, ev.y)
+    //     lf.extension.miniMap.show(
+    //         position.domOverlayPosition.x - 120,
+    //         position.domOverlayPosition.y + 35,
+    //     )
+    //   },
+    //   onClick: (lf, ev) => {
+    //     const position = lf.getPointByClient(ev.x, ev.y)
+    //     lf.extension.miniMap.show(
+    //         position.domOverlayPosition.x - 120,
+    //         position.domOverlayPosition.y + 35,
+    //     )
+    //   },
+    // })
+
+    //设置高亮 path | single
+    this.lf.extension.highlight.setMode('path');
+  },
+};
 </script>
 
 <style scoped>
+.drawArea {
+  width: 100%;
+  height: 95%;
+  margin-top: 2%;
+  border: #333333 solid 1px;
+}
+.container {
+  width: 100%;
+  height: 100%;
+  border: #333333 solid 1px;
+}
 </style>
-
